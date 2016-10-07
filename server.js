bdabad4b198fb3de0f7d4d89374aff8533229793
sandyyo <sandeepharyanajat@gmail.com>
2016-10-07 16:41:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ app.get('/article-three',function(req,res){
 });
 
 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 
